947da78ae6ff44f619b483b44f0216237c30ec32
LizRuelas <liz.ruelas.borda@gmail.com>
2016-08-25 13:49:44 -0700

    modificando app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var numero3;
 var sumaNotas;
 var promedioNotas;
          
-numero1=prompt("Ingrese la primera nota : ","");
-numero2=prompt("Ingrese la segundo nota : ","");
-numero3=prompt("Ingrese la tercer nota : ","");
+numero1=prompt("Ingrese la primera nota : ");
+numero2=prompt("Ingrese la segundo nota : ");
+numero3=prompt("Ingrese la tercer nota : ");
 
 sumaNotas= (parseInt(numero1)+ parseInt(numero2)+parseInt(numero3));
 promedioNotas=sumaNotas/3;
